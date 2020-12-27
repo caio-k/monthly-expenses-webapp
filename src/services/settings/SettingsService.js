1be16f74261aedfaf076bd246c274c699da1a292
caio-k <koiti2000@hotmail.com>
2020-12-27 11:35:21 -0300

    send userId on the settings request header

--- src/services/settings/SettingsService.js
+++ src/services/settings/SettingsService.js
@@ -1,15 +1,10 @@
 import ServerApi from "../api/ServerApi";
-import AuthService from "../auth/AuthService";
 
 const API_PATH = "/api/settings/";
 
 class SettingsService {
   getInitializationData() {
-    return ServerApi.get(API_PATH + 'get', {
-      params: {
-        userId: AuthService.getCurrentUser().id
-      }
-    });
+    return ServerApi.get(API_PATH + 'get');
   }
 }
 
