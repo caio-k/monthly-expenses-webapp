859b284c6c9440ae996b28d254e2ccbb0da975f8
caio-k <koiti2000@hotmail.com>
2020-12-31 11:07:09 -0300

    adjustments on expenses logic

--- src/routes/expenses/expensesLogic.js
+++ src/routes/expenses/expensesLogic.js
@@ -1,9 +1,22 @@
 import {useEffect, useState} from "react";
 import ExpensesService from "../../services/expenses/ExpensesService";
+import useNotification from "../../components/notifications/notification";
 
 const useExpenses = () => {
   const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
     "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+  const createMonthYearObject = (yearNumber, monthNumber) => {
+    if (monthNumber >= 0 && monthNumber <= 11 && yearNumber) {
+      return {
+        monthName: months[parseInt(monthNumber, 10)],
+        monthNumber: parseInt(monthNumber, 10),
+        yearNumber: parseInt(yearNumber, 10)
+      }
+    } else {
+      return {}
+    }
+  }
 
   const [years, setYears] = useState([]);
   const [monthYearsSought, setMonthYearsSought] = useState([]);
@@ -12,27 +25,24 @@ const useExpenses = () => {
   const [initialMoneyList, setInitialMoneyList] = useState([]);
   const [expensesOnFocus, setExpensesOnFocus] = useState([]);
   const [allExpenses, setAllExpenses] = useState([]);
-  const [selectedMonthYear, setSelectedMonthYear] = useState({monthName: "", monthNumber: -1, yearNumber: -1});
+  const [selectedMonthYear, setSelectedMonthYear] = useState(createMonthYearObject());
   const [loadingComponent, setLoadingComponent] = useState(true);
   const [loadingError, setLoadingError] = useState(false);
+  const [handleErrorNotification] = useNotification();
 
   useEffect(() => {
     ExpensesService.getInitializationData().then(
       response => {
-        const selectedMonthYear = {
-          monthName: months[parseInt(response.data.selectedMonth, 10)],
-          monthNumber: parseInt(response.data.selectedMonth, 10),
-          yearNumber: parseInt(response.data.selectedYearNumber, 10)
-        }
+        const selectedMonthYear = createMonthYearObject(response.data.selectedYearNumber, response.data.selectedMonth);
 
         setYears(response.data.years);
         setMonthYearsSought([selectedMonthYear]);
+        setSelectedMonthYear(selectedMonthYear);
         setExpenseTypes(response.data.expenseTypes);
         setInitialMoneyOnFocus(response.data.initialMoney);
         setInitialMoneyList(response.data.initialMoney === null ? [] : [response.data.initialMoney]);
         setExpensesOnFocus(response.data.expenseInfos);
         setAllExpenses(response.data.expenseInfos);
-        setSelectedMonthYear(selectedMonthYear);
         setLoadingComponent(false);
         setLoadingError(false);
       },
@@ -41,17 +51,13 @@ const useExpenses = () => {
         setLoadingError(true);
       }
     )
+    // eslint-disable-next-line
   }, [])
 
   const handleSubmit = (e, year, month) => {
     e.preventDefault();
 
-    const actualMonthYear = {
-      monthName: months[parseInt(month, 10)],
-      monthNumber: parseInt(month, 10),
-      yearNumber: parseInt(year, 10)
-    }
-
+    const actualMonthYear = createMonthYearObject(year, month);
     setSelectedMonthYear(actualMonthYear);
 
     if (checkIfMonthYearAlreadySought(actualMonthYear.yearNumber, actualMonthYear.monthNumber)) {
@@ -62,24 +68,29 @@ const useExpenses = () => {
 
       ExpensesService.getByMonthAndYear(actualMonthYear.monthNumber, actualMonthYear.yearNumber).then(
         response => {
-          if (response.data.initialMoney) {
-            addInitialMoneyOnListAndFocus(response.data.initialMoney);
-          } else {
-            setInitialMoneyOnFocus(null);
-          }
-
+          addInitialMoneyOnListAndFocus(response.data.initialMoney);
           addExpensesOnListAndFocus(response.data.expenseInfos);
         },
-        () => {
-
+        error => {
+          handleErrorNotification(error);
         }
       )
     }
   }
 
+  const checkIfMonthYearAlreadySought = (yearNumber, monthNumber) => {
+    return monthYearsSought.some(element =>
+      element.yearNumber === yearNumber && element.monthNumber === monthNumber
+    );
+  }
+
   const addInitialMoneyOnListAndFocus = (initialMoneyObject) => {
-    setInitialMoneyOnFocus(initialMoneyObject);
-    setInitialMoneyList(list => [...list, initialMoneyObject]);
+    if (initialMoneyObject) {
+      setInitialMoneyOnFocus(initialMoneyObject);
+      setInitialMoneyList(list => [...list, initialMoneyObject]);
+    } else {
+      setInitialMoneyOnFocus(null);
+    }
   }
 
   const updateInitialMoneyOnListAndFocus = (initialMoneyObject) => {
@@ -96,23 +107,17 @@ const useExpenses = () => {
     }
   }
 
-  const addExpensesOnListAndFocus = (expenses) => {
-    setExpensesOnFocus(expenses);
-    setAllExpenses(list => [...list, expenses]);
-  }
-
-  const checkIfMonthYearAlreadySought = (yearNumber, monthNumber) => {
-    return monthYearsSought.some(element =>
-      element.yearNumber === yearNumber && element.monthNumber === monthNumber
-    );
-  }
-
   const setNewInitialMoneyOnFocus = (month, year) => {
     const initialMoneyIndex = initialMoneyList.findIndex(element =>
       element.year === year && element.month === month
     );
 
     setInitialMoneyOnFocus(initialMoneyIndex < 0 ? null : initialMoneyList[initialMoneyIndex]);
+  }
+
+  const addExpensesOnListAndFocus = (expenses) => {
+    setExpensesOnFocus(expenses);
+    setAllExpenses(list => [...list, expenses]);
   }
 
   const setNewExpensesOnFocus = (month, year) => {
