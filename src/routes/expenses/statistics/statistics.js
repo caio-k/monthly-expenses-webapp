e62b1f3ecc47e3a23ae70a0186b7a3a1684e3cb4
caio-k <koiti2000@hotmail.com>
2021-01-26 12:48:16 -0300

    refactor statistics session

--- src/routes/expenses/statistics/statistics.js
+++ src/routes/expenses/statistics/statistics.js
@@ -3,7 +3,7 @@ import useStatistics from "./statisticsLogic";
 import "./statistics.css";
 
 function Statistics(props) {
-  const [emptyChart, moneyAvailable, openExpensesPrice]
+  const [emptyChart, moneyAvailable, totalSpent, openExpensesPrice]
     = useStatistics(props.expensesOnFocus, props.initialMoneyOnFocus, props.expenseTypes);
 
   return (
@@ -15,60 +15,60 @@ function Statistics(props) {
 
       <div className="statistics-container">
         <div className="statistics-box">
-          <div>
-            <h4>Métricas</h4>
-          </div>
-
           {!props.initialMoneyOnFocus && (
             <div className="simple-statistics-message">
-              <p>Cadastre o capital inicial para mais estatísticas!</p>
+              <p>Cadastre o capital inicial para obter métricas!</p>
             </div>
           )}
 
           {props.initialMoneyOnFocus && (
             <div className="statistic-numbers-card">
-              <div style={{backgroundColor: "#48cae4"}}>
-                <p>R${props.initialMoneyOnFocus.initialMoney.toFixed(2)}</p>
-                <p>Capital inicial</p>
+              <div>
+                <div style={{backgroundColor: "#284b63"}}>
+                  <p>R${props.initialMoneyOnFocus.initialMoney.toFixed(2)}</p>
+                  <p>Capital inicial</p>
+                </div>
+
+                {moneyAvailable >= 0 && (
+                  <div style={{backgroundColor: "#06d6a0"}}>
+                    <p>R${moneyAvailable}</p>
+                    <p>Capital disponível</p>
+                  </div>
+                )}
+
+                {moneyAvailable < 0 && (
+                  <div style={{backgroundColor: "#e71d36"}}>
+                    <p>R${moneyAvailable}</p>
+                    <p>Capital excedente</p>
+                  </div>
+                )}
               </div>
 
-              {moneyAvailable >= 0 && (
-                <div style={{backgroundColor: "#06d6a0"}}>
-                  <p>R${moneyAvailable}</p>
-                  <p>Capital disponível</p>
+              <div>
+                <div style={{backgroundColor: "#48cae4"}}>
+                  <p>R${totalSpent}</p>
+                  <p>Despesas pagas</p>
                 </div>
-              )}
 
-              {moneyAvailable < 0 && (
-                <div style={{backgroundColor: "#e71d36"}}>
-                  <p>R${moneyAvailable}</p>
-                  <p>Capital excedente</p>
+                <div style={{backgroundColor: "#ffd166"}}>
+                  <p>R${openExpensesPrice}</p>
+                  <p>Despesas em aberto</p>
                 </div>
-              )}
-
-              <div style={{backgroundColor: "#ffd166"}}>
-                <p>R${openExpensesPrice}</p>
-                <p>Despesas em aberto</p>
               </div>
             </div>
           )}
         </div>
 
         <div className="statistics-box">
-          <div>
-            <h4>Gráfico de Gastos</h4>
-          </div>
-
-          <div style={{width: emptyChart ? "0px" : "330px", height: emptyChart ? "0px" : "330px"}}
-               className="statistics-chart-container">
-            <canvas id="statistics-chart" width="330" height="330"/>
-          </div>
-
           {emptyChart && (
             <div className="simple-statistics-message">
               <p>Oops! Nenhuma despesa paga!</p>
             </div>
           )}
+
+          <div className="statistics-chart-container">
+            <canvas id="statistics-chart" width="400" height="300"/>
+          </div>
         </div>
       </div>
     </div>
--- src/routes/expenses/statistics/statisticsLogic.js
+++ src/routes/expenses/statistics/statisticsLogic.js
@@ -6,23 +6,83 @@ const useStatistics = (expensesOnFocus, initialMoneyOnFocus, expenseTypes) => {
   const [statisticsChart, setStatisticsChart] = useState();
   const [emptyChart, setEmptyChart] = useState(true);
   const [moneyAvailable, setMoneyAvailable] = useState(null);
+  const [totalSpent, setTotalSpent] = useState(null);
   const [openExpensesPrice, setOpenExpensesPrice] = useState(null);
+  const colors = [
+    "rgb(255, 159, 64)",
+    "rgb(255, 205, 86)",
+    "rgb(75, 192, 192)",
+    "rgb(54, 162, 235)",
+    "rgb(255, 99, 132)"
+  ];
 
   useEffect(() => {
+    buildChart();
+    // eslint-disable-next-line
+  }, [expensesOnFocus]);
+
+  function buildChart() {
     if (statisticsChart) {
       statisticsChart.destroy();
     }
 
-    const colors = [
-      "rgb(255, 159, 64)",
-      "rgb(255, 205, 86)",
-      "rgb(75, 192, 192)",
-      "rgb(54, 162, 235)",
-      "rgb(255, 99, 132)"
-    ];
+    const dataset = buildDatasetStructure();
+    const metrics = populateDatasetAndGenerateMetrics(dataset);
 
+    setOpenExpensesPrice(metrics.totalOpenExpenses.toFixed(2));
+    setTotalSpent(metrics.totalSpentAmount.toFixed(2));
+    setEmptyChart(!metrics.hasValidExpenses);
+
+    if (initialMoneyOnFocus) {
+      const initialMoney = initialMoneyOnFocus.initialMoney;
+      setMoneyAvailable((initialMoney - metrics.totalSpentAmount).toFixed(2));
+    }
+
+    adjustLayout(metrics.hasValidExpenses);
+
+    if (metrics.hasValidExpenses) {
+      const labels = [], data = [], backgroundColors = [];
+
+      dataset.forEach(element => {
+        labels.push(element.type);
+        data.push(element.amount.toFixed(2));
+        backgroundColors.push(element.backgroundColor);
+      })
+
+      const statisticsChartInstance = new Chart(document.getElementById('statistics-chart').getContext('2d'), {
+        type: "pie",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: 'Gastos por tipo de despesa',
+              data: data,
+              backgroundColor: backgroundColors
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'Gasto por tipo de despesa',
+            position: 'top',
+            fontColor: 'black',
+            fontFamily: "'Work Sans', sans-serif",
+            fontSize: '14'
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      });
+
+      setStatisticsChart(statisticsChartInstance);
+    }
+  }
+
+  function buildDatasetStructure() {
     let colorCounter = -1;
-    const dataset = expenseTypes.map(expenseType => {
+    return expenseTypes.map(expenseType => {
       colorCounter++;
       return {
         typeId: expenseType.id,
@@ -31,10 +91,13 @@ const useStatistics = (expensesOnFocus, initialMoneyOnFocus, expenseTypes) => {
         backgroundColor: colors[colorCounter % colors.length]
       }
     });
+  }
 
+  function populateDatasetAndGenerateMetrics(dataset) {
     let hasValidExpenses = false;
     let totalSpentAmount = 0;
     let totalOpenExpenses = 0;
+
     expensesOnFocus.forEach(element => {
       if (element.paid === true) {
         hasValidExpenses = true;
@@ -46,47 +109,29 @@ const useStatistics = (expensesOnFocus, initialMoneyOnFocus, expenseTypes) => {
       }
     });
 
-    if (initialMoneyOnFocus) {
-      const initialMoney = initialMoneyOnFocus.initialMoney;
-      setMoneyAvailable((initialMoney - totalSpentAmount).toFixed(2));
+    return {
+      hasValidExpenses: hasValidExpenses,
+      totalSpentAmount: totalSpentAmount,
+      totalOpenExpenses: totalOpenExpenses
     }
+  }
 
-    setOpenExpensesPrice(totalOpenExpenses);
+  function adjustLayout(hasValidExpenses) {
+    const chartContainerElement = document.getElementsByClassName("statistics-chart-container");
+    const chartElement = document.getElementById("statistics-chart");
 
     if (hasValidExpenses) {
-      setEmptyChart(false);
-
-      const labels = [];
-      const data = [];
-      const backgroundColors = [];
-
-      dataset.forEach(element => {
-        labels.push(element.type);
-        data.push(element.amount);
-        backgroundColors.push(element.backgroundColor);
-      })
-
-      const statisticsChartInstance = new Chart(document.getElementById('statistics-chart').getContext('2d'), {
-        type: "pie",
-        data: {
-          labels: labels,
-          datasets: [
-            {
-              label: "Gastos por tipo de despesa",
-              data: data,
-              backgroundColor: backgroundColors
-            }
-          ]
-        }
-      })
-
-      setStatisticsChart(statisticsChartInstance);
+      chartContainerElement[0].classList.remove("statistics-chart-container-empty");
+      chartElement.setAttribute("width", "400px");
+      chartElement.setAttribute("height", "300px");
     } else {
-      setEmptyChart(true);
+      chartContainerElement[0].classList.add("statistics-chart-container-empty");
+      chartElement.setAttribute("width", "0px");
+      chartElement.setAttribute("height", "0px");
     }
-  }, [expensesOnFocus]);
+  }
 
-  return [emptyChart, moneyAvailable, openExpensesPrice];
+  return [emptyChart, moneyAvailable, totalSpent, openExpensesPrice];
 }
 
 export default useStatistics;
