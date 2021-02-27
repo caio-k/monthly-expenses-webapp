6c2cbcfe20d89b7b0795ad76b9e7ac2328d92c72
caio-k <koiti2000@hotmail.com>
2021-02-27 13:06:19 -0300

    create function to get baseURL of request

--- src/services/api/ServerApi.js
+++ src/services/api/ServerApi.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 import authHeader from "./AuthHeader";
 
+const getBaseURL = () => {
+  return window.location.hostname === "https://monthly-expenses-app.herokuapp.com" ?
+    "https://monthly-expenses-server.herokuapp.com" :
+    "http://localhost:8080"
+}
+
 export default axios.create({
-  baseURL: "https://monthly-expenses-server.herokuapp.com",
+  baseURL: getBaseURL(),
   responseType: "json",
   headers: authHeader(),
 });
